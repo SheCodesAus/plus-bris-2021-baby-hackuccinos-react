7e9171a74f7189bd36893bd1a1f8d04e5c8b0aa7
Bel Lloyd <bel@zandiconsulting.com>
2021-11-23 19:32:47 +1000

    adding badges

--- cupcake-coders-app/src/pages/ErrorPage/ErrorPage.jsx
+++ cupcake-coders-app/src/pages/ErrorPage/ErrorPage.jsx
@@ -6,7 +6,7 @@ import error_pic from "../../components/static file/error_pic.png"
 function ErrorPage() {
     return (
     <div><h1>404 Error</h1>
-    <h3>Oh noooo... cupcake crumbles</h3>
+    <h3>Oh noooo... the cupcake crumbles, we didn't find what you were looking for.</h3>
     <img className="error_pic" src={error_pic} alt="errorpage"/>
     
     <Footer/>
