e92ab712e9f19a4e00cf45f2ecd331f73b3455c1
Bel Lloyd <bel@zandiconsulting.com>
2021-11-25 21:42:15 +1000

    Coders Page work

--- cupcake-coders-app/src/pages/CodersPage/CodersPage.jsx
+++ cupcake-coders-app/src/pages/CodersPage/CodersPage.jsx
@@ -5,26 +5,27 @@ import "./CodersPage.css";
 
 
 function CodersPage() {
-    // const [codersList, setCodersList] = useState([]);
+    const [codersList, setCodersList] = useState([]);
 
-    // useEffect(() => {
-    //     fetch(`${process.env.REACT_APP_API_URL}coders/`)
-    //     .then((results) => {
-    //         console.log("This is the results", results)
-    //     return results.json();
-    //     })
-    //     .then ((data) => {
-    //         setCodersList(data);
-    //     });
-    // },[]);
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}coders/`)
+        .then((results) => {
+            console.log("This is the results", results)
+        return results.json();
+        })
+        .then ((data) => {
+            setCodersList(data);
+        });
+    },[]);
 
-    //     return (
-    //     <div id="coders-list">
-    //     {codersList.map((CoderData, key) => {
-    //     return <CoderCard key={key} coderData={CoderData} />;
-    //     })}
-    //     </div>
-    // );
+        return (
+        <div id="coders-list">
+            <h1>This is the coders page.</h1>
+        {codersList.map((CoderData, key) => {
+        return <CoderCard key={key} coderData={CoderData} />;
+        })}
+        </div>
+    );
 return <h1>This is the coders page.</h1>
 }
 
