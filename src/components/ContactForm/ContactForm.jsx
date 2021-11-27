79b1490ea5af5cc4ddbb9c5166e28e7019bea626
mcd1802 <monica.dewi@bhp.com>
2021-11-27 12:14:35 +1000

    Updating the contact form
    Updating the contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,84 +2,43 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./ContactForm.css";
 
-function ContactForm () {
-
-    const [coderData, setCoderData] = useState({});
-    const navigate = useNavigate();
-
-    const handleChange = (e) => {
-        let { id, value } = e.target;
-        setCoderData((prevCoderData) => ({
-            ...prevCoderData,
-            [id]: value, 
-        }));
-
-    };
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-
-
-        const response = await fetch(
-            `${process.env.REACT_APP_API_URL}coders/`, 
-            {
-            method: "post",
-            headers: {
-                "Authorization": `Token ${localStorage.getItem('token')}`,
-                "Content-type": "application/json",
-            },
-            body: JSON.stringify(coderData),
-            }
-        );
-    }
-
+function ContactForm () {    
     return (
+        
         <contact>
+            <form action="https://formspree.io/f/mknkblol" method="post"></form>
             <div class="label">
                 <label htmlFor="name">Name</label>
                 <input
-                onChange={handleChange}
                 type="text"
-                id="current_role"
                 placeholder="Name"
-                value={coderData.current_role}
                 />
             </div>
             <div class="label">
                 <label htmlFor="Email address">Email address</label>
                 <input
-                onChange={handleChange}
                 type="text"
-                id="current_role"
                 placeholder="Email"
-                value={coderData.current_role}
                 />
             </div>
             <div class="label">
                 <label htmlFor="Phone Number">Phone Number</label>
                 <input
-                onChange={handleChange}
                 type="text"
-                id="current_role"
                 placeholder="Phone"
-                value={coderData.current_role}
                 />
             </div>
             
             <div class="label">
                 <label htmlFor="Message">Message</label>
                 <input
-                onChange={handleChange}
                 type="text"
-                id="current_role"
                 placeholder="Message"
-                value={coderData.current_role}
                 />
             </div>
             
             <div className="button-centre">
-            <button onClick={handleSubmit} type="submit">
+            <button type="submit">
             Order submitted!
             </button>
             </div>
