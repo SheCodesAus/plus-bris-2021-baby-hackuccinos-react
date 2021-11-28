8628e44020d2af241f089d19054c2e026e087b89
Bel Lloyd <bel@zandiconsulting.com>
2021-11-28 15:40:02 +1000

    Count-up-animation

--- src/components/ImpactCounter/ImpactCounter.jsx
+++ src/components/ImpactCounter/ImpactCounter.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./ImpactCounter.css";
 
 function ImpactCounter () {
+    
+    const easeOutQuad = t => t * ( 2 - t );
+    const frameDuration = 1000 / 60;
+
+    const CountUpAnimation = ( { children, duration = 2000 } ) => {
+	const countTo = parseInt( children, 10 );
+	const [ count, setCount ] = useState( 100 );
+
+	useEffect( () => {
+		let frame = 0;
+		const totalFrames = Math.round( duration / frameDuration );
+		const counter = setInterval( () => {
+			frame++;
+			const progress = easeOutQuad( frame / totalFrames );
+			setCount( countTo * progress );
+
+			if ( frame === totalFrames ) {
+				clearInterval( counter );
+			}
+		}, frameDuration );
+	}, [] );
+
+	return Math.floor( count );
+    };
+    
     return (
         <div class='counter'>
             <div class="impact-circle" onclick="location.href='https://shecodes.com.au/partner-with-us/">
                 <div class="circle">
                     <div class="number">
-                        <h2>4065</h2>
+                        <CountUpAnimation>4065</CountUpAnimation>
                     </div>
                 </div>
                 <div class="impact-label">
@@ -19,7 +44,7 @@ function ImpactCounter () {
             <div class="impact-circle">
                 <div class="circle">
                     <div class="number">
-                        <h2>290</h2>
+                        <CountUpAnimation>290</CountUpAnimation>
                     </div>
                 </div>
                 <div class="impact-label">
@@ -30,7 +55,7 @@ function ImpactCounter () {
             <div class="impact-circle">
                 <div class="circle">
                     <div class="number">
-                        <h2>128</h2>
+                        <CountUpAnimation>128</CountUpAnimation>
                     </div>
                 </div>
                 <div class="impact-label">
