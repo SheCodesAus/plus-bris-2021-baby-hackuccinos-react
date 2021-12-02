cc860bbcc8065f4941578931190a8652ef2d74b5
Bel Lloyd <bel@zandiconsulting.com>
2021-12-02 13:54:46 +1000

    partners logos and styling

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -6,8 +6,18 @@ function Partners () {
 
     return (
         <div class="partners">
-            <img src="https://raw.githubusercontent.com/bel-lloyd/my-files/master/BHP_2017_logo.svg.png"></img>
-            <img src="https://raw.githubusercontent.com/bel-lloyd/my-files/master/square_logo_raster%404x-e1611290037349.png"></img>
+            <div class="partners-logo">
+                <img src="https://raw.githubusercontent.com/bel-lloyd/my-files/master/BHP_2017_logo.svg.png"/>
+            </div>
+            <div class="partners-logo">
+                <img src="https://raw.githubusercontent.com/bel-lloyd/my-files/master/your-partner-logo-here.png"></img>
+            </div>
+            <div class="partners-logo">
+                <img src="https://raw.githubusercontent.com/bel-lloyd/my-files/master/square_logo_raster%404x-e1611290037349.png"></img>
+            </div>
+            <div class="partners-logo">
+                <img src="https://raw.githubusercontent.com/bel-lloyd/my-files/master/Bunnings_Warehouse-Logo.wine.png"></img>
+            </div>
         </div>
 );
 }
