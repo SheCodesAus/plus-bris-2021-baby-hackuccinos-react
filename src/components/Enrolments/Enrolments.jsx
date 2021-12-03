fd722ab5cf2359a8520cc0f8700c4d1e2ee438f7
Bel Lloyd <bel@zandiconsulting.com>
2021-12-03 21:05:32 +1000

    Impact Page changes and styling

--- src/components/Enrolments/Enrolments.jsx
+++ src/components/Enrolments/Enrolments.jsx
@@ -4,15 +4,26 @@ import enrolments from "../../components/static file/enrolments.jpeg";
 
 function Enrolments() {
   return (
-    <div class="intro">
-      <h1>Enrolments</h1>
-      <div class="impact-row">
-        <p>
-          In the last 5 years, She Codes is proud to have taught 4025 women
-          across Australia in a variation of languages: HTML, CSS, Wordpress,
-          Python and Django. To find out more about courses She Codes provides{" "}
-          <a href="https://shecodes.com.au/learn/"> click here.</a>{" "}
-        </p>
+    <div class="main-enrol">
+      <div class="title-txt">
+        <div class="title">
+          <h1>Enrolments</h1>
+        </div>
+        <div class="txt-left">
+          <p>
+            In the last 5 years, She Codes is proud to have taught 4025 women
+            across Australia in a variation of languages: HTML, CSS, Wordpress,
+            Python and Django. To find out more about courses She Codes provides{" "}
+            <a href="https://shecodes.com.au/learn/"> click here.</a>{" "}
+          </p>
+        </div>
+        <div id="button-div">
+          <a href to="https://shecodes.com.au/learn/">
+            <button class="section-button">Learn More</button>
+          </a>
+        </div>
+      </div>
+      <div class="img">
         <img className="impact-img" src={enrolments} alt="Enrolments" />
       </div>
     </div>
--- src/components/TechPursuedRole/TechPursuedRole.jsx
+++ src/components/TechPursuedRole/TechPursuedRole.jsx
@@ -5,14 +5,26 @@ import techrole from "../../components/static file/techrole.png";
 
 function TechPursuedRole() {
   return (
-    <div class="intro">
-      <h1>Tech Roles</h1>
-      <div class="impact-row">
-        <p>
-          To date, 290 students who have studied with She Codes have pursued a
-          Technology Career, as a direct result of completing a She Codes
-          Course.
-        </p>
+    <div class="main-enrol">
+      <div class="title-txt">
+        <div class="title">
+          <h1>Tech Roles</h1>
+        </div>
+        <div class="txt-left">
+          <p>
+            To date, 290 students who have studied with She Codes have pursued a
+            Technology Career, as a direct result of completing a She Codes
+            Course. Get in touch with our team today to see how we could help you
+            step into your tech career. 
+          </p>
+        </div>
+        <div id="button-div">
+          <a href mailto="katebushell@shecodes.com.au">
+            <button class="section-button">Get in touch</button>
+          </a>
+        </div>
+      </div>
+      <div class="img">
         <img className="impact-img" src={techrole} alt="TechPursuedRole" />
       </div>
     </div>
